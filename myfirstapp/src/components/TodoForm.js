359be27c6f14019dc3e273a03efa995b1bf71a8f
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-02-16 14:00:43 -0600

    created ternarios

--- myfirstapp/src/components/TodoForm.js
+++ myfirstapp/src/components/TodoForm.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class TodoForm extends Component {
   constructor (props) {
-    super();
+    console.log('soy bob el constructor')
+    super(props);
     this.state = {
       title: '',
       responsible: '',
@@ -12,7 +13,8 @@ class TodoForm extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-sa
+
+
 
 
   handleSubmit(e) {
@@ -36,43 +38,87 @@ sa
 
   render() {
     // console.log ("state del TodoForm --------------------->", this.state);
-    console.log ("prop del TodoForm----------------------->", this.props);
+    // console.log ("prop del TodoForm----------------------->", this.props);
+    console.log ("soy el render-")
 
     return (
       <div className="card">
         <form onSubmit={this.handleSubmit} className="card-body">
           <div className="form-group">
-          <input type="hidden" id="index" name="index" />
-            <input
-              type="text"
-              name="title"
-              className="form-control"
-              value={this.state.title}
-              onChange={this.handleInputChange}
-              placeholder="Title"
-              />
+          <input type="hidden" id="index" name="index" value={this.props.indexUpdate} />
+
+            {this.props.titleUpdate === "" ?
+             <input
+               type="text"
+               name="title"
+               className="form-control"
+               value={this.state.title}
+               onChange={this.handleInputChange}
+               placeholder="Title"
+             />
+               :
+               <input
+                 type="text"
+                 name="title"
+                 className="form-control"
+                 value={this.props.titleUpdate}
+                 onChange={this.handleInputChange}
+                 placeholder="Title"
+               />
+            }
+
           </div>
           <div className="form-group">
-            <input
-              type="text"
-              name="responsible"
-              className="form-control"
-              value={this.state.responsible}
-              onChange={this.handleInputChange}
-              placeholder="Responsible"
-              />
+          <input type="hidden" id="index" name="index" value={this.props.indexUpdate} />
+
+            {this.props.ResponsibleUpdate === "" ?
+             <input
+               type="text"
+               name="title"
+               className="form-control"
+               value={this.state.responsible}
+               onChange={this.handleInputChange}
+               placeholder="Responsible"
+             />
+               :
+               <input
+                 type="text"
+                 name="responsible"
+                 className="form-control"
+                 value={this.props.responsibleUpdate}
+                 onChange={this.handleInputChange}
+                 placeholder="Responsible"
+               />
+            }
+          </div>
+
+          <div className="form-group">
+          <input type="hidden" id="index" name="index" value={this.props.indexUpdate} />
+
+            {this.props.DescriptionUpdate === "" ?
+             <input
+               type="text"
+               name="description"
+               className="form-control"
+               value={this.state.description}
+               onChange={this.handleInputChange}
+               placeholder="Description"
+             />
+               :
+               <input
+                 type="text"
+                 name="description"
+                 className="form-control"
+                 value={this.props.descriptionUpdate}
+                 onChange={this.handleInputChange}
+                 placeholder="Description"
+               />
+            }
           </div>
           <div className="form-group">
-            <input
-              type="text"
-              name="description"
-              className="form-control"
-              value={this.state.description}
-              onChange={this.handleInputChange}
-              placeholder="Description"
-              />
-          </div>
-          <div className="form-group">
+          <input type="hidden" id="index" name="index" value={this.props.indexUpdate} />
+
+            {this.props.priorityUpdate === "" ?
             <select
                 name="priority"
                 className="form-control"
@@ -83,10 +129,34 @@ sa
               <option>medium</option>
               <option>high</option>
             </select>
+               :
+               <select
+                   name="priority"
+                   className="form-control"
+                   value={this.props.priorityUpdate}
+                   onChange={this.handleInputChange}
+                 >
+                 <option>low</option>
+                 <option>medium</option>
+                 <option>high</option>
+               </select>
+
+            }
           </div>
-          <button type="submit" className="btn btn-primary">
-            Save
-          </button>
+
+          { this.props.indexUpdate === "" ?
+              <button type="submit" className="btn btn-primary">
+                Save
+              </button>
+              :
+              <button type="submit" className="btn btn-success">
+                Update
+              </button>
+          }
+
+
+
+
           {JSON.stringify(
             this.state
           )}
@@ -98,6 +168,7 @@ sa
       </div>
     )
   }
+
 
 }
 
