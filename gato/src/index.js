74bf9f8118672bb3c97aa8ae07f2993c8b3776eb
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-02-28 15:18:10 -0600

    created the function en medio

--- gato/src/index.js
+++ gato/src/index.js
@@ -6,20 +6,38 @@ import styled, {css}from 'styled-components'
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    var num1 = 35,
+        num2 = 50,
+        num3 = 45,
+        result= (num1>num3)?
+          num1
+          :
+          num3;
+    var    result2 = (num2>num3)?
+        num2
+        :
+        num3;
+    var result3 = (result<result2)?
+    result
+    :
+    result2;
+
+        console.log("el numero de en medio es--->",result3);
+
 
   }
 
 
-  render() {
 
+
+  render() {
     return (
       <div>
-      jjj
+
       </div>
     );
   }
 }
-
 
 
 // ========================================
