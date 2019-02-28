49182a67e842d6e6fea250ebdcdde7af16c7f483
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-02-28 16:02:12 -0600

    created the interface and the function again

--- gato/src/index.js
+++ gato/src/index.js
@@ -3,36 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import styled, {css}from 'styled-components'
 
+
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    var num1 = 35,
-        num2 = 50,
-        num3 = 45,
-        result= (num1>num3)?
-          num1
-          :
-          num3;
-    var    result2 = (num2>num3)?
-        num2
-        :
-        num3;
-    var result3 = (result<result2)?
-    result
-    :
-    result2;
+  constructor() {
+    super();
+    this.state = {
+        num1: '' ,
+        num2: '',
+        num3: ''
+      }
+        this.handleComparativa = this.handleComparativa.bind(this);
+}
+handleComparativa(num1, num2, num3 ){
 
-        console.log("el numero de en medio es--->",result3);
-
-
+        result= (num1>num3) ? num1 : num3;
+        result2 = (num2>num3) ? num2 : num3;
+      return  result3 = (result<result2) ? result : result2;
   }
 
-
+        //console.log("el numero de en medio es--->",result3);
 
 
   render() {
+
     return (
       <div>
+      <form>
+
+      <h5> Primer valor </h5>
+      <input type="text" name="num1"  />
+
+      <h5> Segundo valor </h5>
+      <input type="text" name="num2"  />
+
+      <h5> Tercer valor </h5>
+      <input type="text" name="num3"  />
+
+      <button onClick=handleComparativa> Comparar </button>
+
+      <h5> Resultado </h5>
+      <input type="text" name="result3" />
+      </form>
 
       </div>
     );
