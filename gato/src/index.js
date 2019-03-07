a6c9978bcaadf6aa846a8595f75f183126c983c4
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-03-07 17:22:36 -0600

    part 2

--- gato/src/index.js
+++ gato/src/index.js
@@ -6,38 +6,78 @@ import styled, {css}from 'styled-components'
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    var letra = 'C';
-    var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
-     'R','S','T','U','V','W','X','Y','Z'];
+    this.state = {
+    result:''
+   }
+   this.handleComparativa = this.handleComparativa.bind(this);
+ }
 
-    // console.log(abecedario[3]);
+   handleComparativa(e){
 
-     abecedario.forEach(function(element, index) {
-       //console.log(element);
 
-       if(letra == element){
-         console.log(abecedario[index+1]);
-       }else{
-         (abecedario>26)
-         console.log(abecedario[0]);
+
+   console.log(e.keyCode);
+
+
+    // console.log("evento-->",e);
+    // console.log("target o input--->",e.target.value);
+     var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
+      'R','S','T','U','V','W','X','Y','Z'];
+
+     abecedario.forEach((element, index)=> {
+
+
+       if(e.target.value == element){
+         console.log("primer validacion-->");
+         if(index==26) {
+           this.setState({
+             result:abecedario[0]
+           })
+          console.log("segunda validacion-->");
+          console.log(abecedario[0]);
+         return
        }
 
-         //console.log(element);
-        // console.log(index+1):
+          this.setState({
+            result:abecedario[index+1]
+          });
+          console.log(abecedario[index+1]);
+     }
 
+     if (e.keyCode == 8){
+
+
+     }
 
      });
 
+
+
    }
 
+   // handleSubmit(e) {
+   //     e.preventDefault();
+   //     this.setState({
+   //       letra:'',
+   //       result:''
+   //     });
+   //   }
 
+
+     handleInputChange(e) {
+       const {value, name} = e.target;
+       // console.log(value, name);
+       this.setState({
+         [name]: value
+       });
+   }
 
 
   render() {
 
     return (
       <div>
-
+      <textarea value={this.state.result} onKeyUp={this.handleComparativa} name="texto">  </textarea>
       </div>
     );
   }
