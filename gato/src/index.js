6b53c01aea1c6c18d06f7df663d20d35def20020
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-03-12 16:57:18 -0600

    x

--- gato/src/index.js
+++ gato/src/index.js
@@ -16,38 +16,48 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    result:''
+    result:'',
+    li:''
    }
    this.handleComparativa = this.handleComparativa.bind(this);
+   // this.handleSubmit = this.handleSubmit.bind(this);
+   // this.handleInputChange = this.handleInputChange.bind(this);
  }
 
-   handleComparativa(e){
-
-
-
-   console.log(e.keyCode);
-
+   handleComparativa(e,result){
 
     // console.log("evento-->",e);
     // console.log("target o input--->",e.target.value);
      var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
       'R','S','T','U','V','W','X','Y','Z',e.keyUp==26];
+      var p = e.target.value.split('');
+      console.log("p-->",p);
+      p.forEach((element)=>{
+
+
+
+
+      })
+
      abecedario.forEach((element, index)=> {
 
+     letra abc ? (index==26) letra:abecedario[0] : l1:abecedario[index+1]
 
        if(e.target.value == element){
          console.log("primer validacion-->");
          if(index==26) {
            this.setState({
-             result:abecedario[0]
+             letra:abecedario[0]
            })
           // console.log("segunda validacion-->");
           // console.log(abecedario[0]);
          return
        }
           this.setState({
-            result:abecedario[index+1]
+            result:e.onChange,
+            l1:abecedario[index+1]
           });
+
           console.log(abecedario[index+1]);
 
      }
@@ -65,35 +75,37 @@ class Game extends React.Component {
    // handleSubmit(e) {
    //     e.preventDefault();
    //     this.setState({
-   //       letra:'',
    //       result:''
    //     });
    //   }
 
 
-     handleInputChange(e) {
-       const {value, name} = e.target;
-       // console.log(value, name);
-       this.setState({
-         [name]: value
-       });
-   }
+   //   handleInputChange(e) {
+   //     const {value, name} = e.target;
+   //     // console.log(value, name);
+   //     this.setState({
+   //       [name]: value
+   //     });
+   // }
 
 
   render() {
 
     return (
+      <form >
       <div>
-      <form>
-      <textarea value={this.state.result} onChange={this.handleComparativa} name="texto">  </textarea>
+      <textarea value={this.state.result} onChange={this.handleComparativa}id="textareabox" name="textarea1" placeholder="Start here..."></textarea>
 
 
-      <Button
-      type="submit">
-      Palabra Nueva
-      </Button>
-      </form>
+      <input
+        type="text"
+        name="num3"
+        value={this.state.l1}
+        onChange={this.handleComparativa}
+        />
+
       </div>
+      </form>
     );
   }
 }
