e8a7f90cd82ad42c6eb4928ce472c838e41687f2
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-03-13 15:05:41 -0600

    space in the input whit ' '

--- gato/src/index.js
+++ gato/src/index.js
@@ -11,6 +11,17 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
+const Input = styled.input.attrs(({ size }) => ({
+  margin: size || "1em",
+  padding: size || "1em"
+}))`
+  color:black ;
+  font-size: 1em;
+  border: 2px solid orange;
+  border-radius: 3px;
+`;
+
+
 
 class Game extends React.Component {
   constructor(props) {
@@ -20,95 +31,73 @@ class Game extends React.Component {
     li:''
    }
    this.handleComparativa = this.handleComparativa.bind(this);
-   // this.handleSubmit = this.handleSubmit.bind(this);
-   // this.handleInputChange = this.handleInputChange.bind(this);
  }
 
    handleComparativa(e,result){
 
-    // console.log("evento-->",e);
-    // console.log("target o input--->",e.target.value);
      var abecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q',
-      'R','S','T','U','V','W','X','Y','Z',e.keyUp==26];
-      var palabra = e.target.value.split('');
-      console.log("p-->",palabra);
-      var resultadoFinal='';
-      palabra.forEach((letra) =>{
+      'R','S','T','U','V','W','X','Y','Z',e.keyUp==26,'  '];
+     var palabra = e.target.value.split("");
+        console.log("p-->",palabra);
+     var resultadoFinal='';
+     var espacio="";
+        palabra.forEach((letra) =>{
 
-        // var letraAbc = p[index]
-        // console.log("letra-->",letraAbc);
-
-     abecedario.forEach((element, index)=> {
+      abecedario.forEach((element, index)=> {
 
 
        if(letra == element){
 
-
          console.log("primer validacion-->");
-         if(index==26) {
+          if(index==26) {
           resultadoFinal += abecedario[0]
-          this.setState ({
-            li:resultadoFinal
+            this.setState ({
+              li:resultadoFinal
           })
 
          // console.log("segunda validacion-->");
          //   console.log(abecedario[0]);
          return
-       }
-         resultadoFinal += abecedario[index+1]
-         this.setState({
-          result:e.onChange,
-            l1:resultadoFinal
+        }
+           resultadoFinal  += abecedario[index+1]
+            this.setState({
+              result:e.onChange,
+              l1:resultadoFinal
           });
 
-          console.log(abecedario[index+1]);
+          console.log(abecedario[index-1]);
 
      }
 
-      if (e.keyUp==8){
-
-      }
        });
 
      });
 
-
-
    }
-
-   // handleSubmit(e) {
-   //     e.preventDefault();
-   //     this.setState({
-   //       result:''
-   //     });
-   //   }
-
-
-   //   handleInputChange(e) {
-   //     const {value, name} = e.target;
-   //     // console.log(value, name);
-   //     this.setState({
-   //       [name]: value
-   //     });
-   // }
 
 
   render() {
 
     return (
       <form >
-      <div>
-      <textarea value={this.state.result} onChange={this.handleComparativa}id="textareabox" name="textarea1" placeholder="Start here..."></textarea>
+        <div>
+          <textarea
+          value={this.state.result}
+          onChange={this.handleComparativa}
+          id="textareabox"
+          name="textarea1"
+          placeholder="Start here...">
+          </textarea>
 
 
-      <input
-        type="text"
-        name="num3"
-        value={this.state.l1}
-        onChange={this.handleComparativa}
-        />
+          <Input
+            type="text"
+            name="num3"
+            value={this.state.l1}
+            onChange={this.handleComparativa}
+            />
 
-      </div>
+        </div>
       </form>
     );
   }
